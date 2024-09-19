0e66f9b38fee16d5f855634f6332923778cea772
ekistoclecio <ekistoclecio1999@gmail.com>
2024-09-18 22:54:23 -0300

    Initial jest tests

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: {
+                node: 'current'
+              }
+            }
+          ],
+          '@babel/preset-react',
+          '@babel/preset-typescript'
+        ],
+        plugins: ['dynamic-import-node']
+      }
+    ]
+  }
+}
